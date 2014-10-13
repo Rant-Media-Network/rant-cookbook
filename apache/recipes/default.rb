2d263d25f8247c0c484a5bf4f54ded7ade7adc6c
hussainpulav <hussainpulav>
2014-10-13 12:25:47 -0700

    apache

---
+++ apache/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "httpd" do
+	action :install
+end
+
+service "httpd" do
+	action [:enable, :start]
+end
+
+template "/etc/apache2/apache2.conf" do
+	source "apache2.conf.erb"
+end
