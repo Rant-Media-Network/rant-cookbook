bda120d4ff8033165a1f7754a86637d2ac3fba1e
hussainpulav <hussainpulav>
2014-10-13 16:40:51 -0700

    apache

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -6,14 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "httpd" do
-	action :install
-end
+
+#package "httpd" do
+#	action :install
+#end
 
 template "/etc/apache2/apache2.conf" do
 	source "apache2.conf.erb"
 end
 
-service "httpd" do
-	action [:enable, :start]
-end
+#service "httpd" do
+#	action [:enable, :start]
+#end
