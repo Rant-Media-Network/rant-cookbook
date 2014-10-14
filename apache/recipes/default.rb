8e9e5e1a50180258dc3e5c13706effbd6a612041
hussainpulav <hussainpulav>
2014-10-14 14:48:27 -0700

    apache

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -10,11 +10,25 @@
 #package "httpd" do
 #	action :install
 #end
+package "php-apc" do
+	action :install
+end
 
 template "/etc/apache2/apache2.conf" do
 	source "apache2.conf.erb"
 end
 
+template "/etc/apache2/mods-available/mpm_prefork.conf.erb" do
+	source "mpm_prefork.conf.erb"
+end
+
+execute "a2enmod-rewrite" do
+	command "/usr/sbin/a2enmod rewrite"
+action :nothing
+only_if {node['platform_family'] == "ubuntu"}
+creates "/etc/apache2/mods-enabled rewrite.load"
+end
+
 service "apache2" do
-	action [:enable, :restart]
+	action :restart
 end
