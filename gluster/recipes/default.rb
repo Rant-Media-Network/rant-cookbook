1aeed6fd73414579d45a66844d7c209322da3360
hussainpulav <hussainpulav>
2014-10-20 14:36:43 -0700

    gluster

--- gluster/recipes/default.rb
+++ gluster/recipes/default.rb
@@ -17,3 +17,18 @@ bash "download fuse-utils" do
 bash "install fuse-utils" do
 	code "dpkg -i fuse-utils_2.9.0-1ubuntu3_all.deb"
 end
+
+bash "download glusterfs-common_3.2.5" do
+    code "wget http://mirrors.kernel.org/ubuntu/pool/universe/g/glusterfs/glusterfs-common_3.2.5-1ubuntu1_amd64.deb"
+  end
+
+bash "install glusterfs-common_3.2.5" do
+	code "dpkg -i glusterfs-common_3.2.5-1ubuntu1_amd64.deb"
+end
+bash "download glusterfs-client_3.2.5" do
+    code "wget http://mirrors.kernel.org/ubuntu/pool/universe/g/glusterfs/glusterfs-client_3.2.5-1ubuntu1_amd64.deb"
+  end
+
+bash "install glusterfs-client_3.2.5" do
+	code "dpkg -i glusterfs-client_3.2.5-1ubuntu1_amd64.deb"
+end
