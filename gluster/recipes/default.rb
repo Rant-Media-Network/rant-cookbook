91539a9915bc6687935a8e78bc2446b35c94499f
hussainpulav <hussainpulav>
2014-10-20 14:17:33 -0700

    gluster

--- gluster/recipes/default.rb
+++ gluster/recipes/default.rb
@@ -6,3 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package "libibverbs1" do
+	action :install
+end
+
+bash "download fuse-utils" do
+    code "wget http://launchpadlibrarian.net/134821398/fuse-utils_2.9.0-1ubuntu3_all.deb"
+  end
+
+bash "install fuse-utils" do
+	code "dpkg -i fuse-utils_2.9.0-1ubuntu3_all.deb"
+end
